80196e9b6bd14c8ecf11d214677cea3792bdeef4
Jose Alvarez <g00463862@atu.ie>
2026-02-24 12:01:13 +0000

    defining service file

--- src/main/java/ie/atu/lab4validation/module/Product.java
+++ src/main/java/ie/atu/lab4validation/module/Product.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.xml.validation.Schema;
+
 @Getter
 @Setter
 public class Product {
-    //@Schema(accessMode = Schema.Access.Mode.READ_ONLY)
-    //private Long id;
+    //@Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    private Long id;
 
     @NotBlank(message = "Product name required")
     private String name;
--- src/main/java/ie/atu/lab4validation/service/ProductService.java
+++ src/main/java/ie/atu/lab4validation/service/ProductService.java
@@ -1,4 +1,20 @@
 package ie.atu.lab4validation.service;
 
+import ie.atu.lab4validation.module.Product;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ProductService {
+    private final List<Product> products = new ArrayList<>();
+    private long nextId = 1;
+
+    public Product addProduct(Product product){
+        product.setId(nextId++);
+        products.add(product);
+        return product;
+    }
 }
