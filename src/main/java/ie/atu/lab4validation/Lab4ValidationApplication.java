336f7fcfe051d38036fb4bab302d08139565c35f
Jose Alvarez <g00463862@atu.ie>
2026-02-24 11:08:32 +0000

    new project, Had to start again

---
+++ src/main/java/ie/atu/lab4validation/Lab4ValidationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab4validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4ValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4ValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/lab4validation/Lab4ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab4validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4ValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
