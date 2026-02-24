2025ba50c47d96c2ca93b7996703094a40fce23d
Jose Alvarez <g00463862@atu.ie>
2026-02-24 11:38:14 +0000

    module file added + (more packages & file for service)

---
+++ src/main/java/ie/atu/lab4validation/module/Product.java
@@ -0,0 +1,20 @@
+package ie.atu.lab4validation.module;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    //@Schema(accessMode = Schema.Access.Mode.READ_ONLY)
+    //private Long id;
+
+    @NotBlank(message = "Product name required")
+    private String name;
+
+    @Positive(message = "Price must be greater than zero")
+    private double price;
+}
+}
